1e23516a0fbcdc226333fedf85df17115e4d1ca8
Jani Saloranta <jiisaloran@gmail.com>
2020-09-09 13:46:19 +0300

    Update problem 152

--- struct2namedCell.m
+++ struct2namedCell.m
@@ -2,8 +2,5 @@
 % Problem 152. Create a cell array out of a struct
 
 function C = struct2namedCell(S)
-  N = fieldnames(S);
-  for ii = 1:length(N)
-     C(ii,:) = [N(ii), {S.(N{ii})}];
-  end
+  C = [fieldnames(S) struct2cell(S)];
 end
