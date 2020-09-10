b0166e064f7ee5f1035d521dc53bd423869f2f6c
Jani Saloranta <jiisaloran@gmail.com>
2020-09-10 12:02:36 +0300

    Update euler001.m

--- euler001.m
+++ euler001.m
@@ -1,8 +1,11 @@
+% Matlab Cody
+% Problem 230. Project Euler: Problem 1, Multiples of 3 and 5
+
 function y = euler001(x)
+  % Find the sum of all the multiples of 3 or 5 below the input value.
+  y = sum(unique([3:3:x-1 5:5:x-1]));
+
   %X = 1:(x-1);
   %y = sum(unique([X(mod(X,3)==0) X(mod(X,5)==0)]));
-  
-  y = sum(unique([3:3:x-1 5:5:x-1]));
-  
-  
+
 end
