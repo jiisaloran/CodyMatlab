3a0f79bb9ae4930ade13750a1214908465b613d1
Jani Saloranta <jiisaloran@gmail.com>
2020-09-10 12:03:59 +0300

    Update euler007.m

--- euler007.m
+++ euler007.m
@@ -1,13 +1,16 @@
+% Matlab Cody
+% Problem 241. Project Euler: Problem 7, Nth prime
+% What is the Nth prime number?
 function y = euler007(x)
-p=3;
-c=1;
+p = 3;
+c = 1;
 while 1
    if isprime(p)
-      c=c+1;
+      c = c + 1;
       if c==x
-         y=p;
+         y = p;
          return;
       end
    end
-   p=p+2;
+   p = p + 2;
 end
