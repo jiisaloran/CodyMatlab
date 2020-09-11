68c9ca6b32c4189effd8006e06db8ef6b870ac66
Jani Saloranta <jiisaloran@gmail.com>
2020-09-11 13:37:13 +0300

    One liner for 967

--- break_string.m
+++ break_string.m
@@ -2,13 +2,6 @@
 % Problem 967. Split a string into chunks of specified length
 function y = break_string(s, b)
 
-if sum(b) == length(s)
-   y = mat2cell(s,[1], b);
-else
-   % Extra characters leftover at the end should be dropped.
-   b(end+1) = length(s) - sum(b);
-   y = mat2cell(s,[1], b);
-   y(end) = [];
-end
+   y = mat2cell(s(1:sum(b)), [1], b);
 
 end
